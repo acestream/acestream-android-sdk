33a59a53700c29ea3af1dfa3a7966c5d8073d77b
Anton <apishel@gmail.com>
2019-04-26 11:14:38 +0300

    feat: add 'send_debug_reports' config param

--- src/main/java/org/acestream/sdk/controller/api/response/AndroidConfig.java
+++ src/main/java/org/acestream/sdk/controller/api/response/AndroidConfig.java
@@ -3,4 +3,5 @@ package org.acestream.sdk.controller.api.response;
 public class AndroidConfig {
     public AndroidNotification[] notifications;
     public AdConfig ad_config;
+    public boolean send_debug_reports = false;
 }
