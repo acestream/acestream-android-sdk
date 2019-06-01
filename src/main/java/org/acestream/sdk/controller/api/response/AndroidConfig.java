e7fe9f299ee599af1e2687c3aed5517cc93f0586
Anton <apishel@gmail.com>
2019-06-01 15:17:39 +0300

    chore: add 'use_acknowledged_purchase_cache' remote param

--- src/main/java/org/acestream/sdk/controller/api/response/AndroidConfig.java
+++ src/main/java/org/acestream/sdk/controller/api/response/AndroidConfig.java
@@ -4,4 +4,5 @@ public class AndroidConfig {
     public AndroidNotification[] notifications;
     public AdConfig ad_config;
     public boolean send_debug_reports = false;
+    public boolean use_acknowledged_purchase_cache = false;
 }
